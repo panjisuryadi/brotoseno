4f2747709dce769c40f193bffaa3ea8b9d36b4c7
galaxyruzhar <galaxyruzhar@gmail.com>
2025-06-23 08:32:04 +0700

    eds

--- resources/views/products/list_baki.blade.php
+++ resources/views/products/list_baki.blade.php
@@ -169,7 +169,7 @@
         searching: true,
         "oLanguage": {
             "sSearch": "<i class='bi bi-search'></i> {{ __("labels.table.search") }} : ",
-            "sLengthMenu": "_MENU_ &nbsp;&nbsp;Data Per {{ __("labels.table.page") }} ",
+            "sLengthMenu": "Data Page;_MENU_ &nbsp;&nbsp",
             "sInfo": "{{ __("labels.table.showing") }} _START_ s/d _END_ {{ __("labels.table.from") }} <b>_TOTAL_ data</b>",
             "sInfoFiltered": "(filter {{ __("labels.table.from") }} _MAX_ total data)",
             "sZeroRecords": "{{ __("labels.table.not_found") }}",
@@ -190,7 +190,7 @@
         ],
         "sPaginationType": "simple_numbers",
         ajax: '/sale/index_data/{{$id}}',
-        dom: 'lfrtip',
+          dom: '<"row"<"col-md-2"l><"col-md-6"f>>rtip',
         // dom: 'Blfrtip',
         columns: [{
             "data": 'id',
@@ -256,7 +256,7 @@
         searching: true,
         "oLanguage": {
             "sSearch": "<i class='bi bi-search'></i> {{ __("labels.table.search") }} : ",
-            "sLengthMenu": "_MENU_ &nbsp;&nbsp;Data Per {{ __("labels.table.page") }} ",
+            "sLengthMenu": "Data Page;_MENU_ &nbsp;&nbsp",
             "sInfo": "{{ __("labels.table.showing") }} _START_ s/d _END_ {{ __("labels.table.from") }} <b>_TOTAL_ data</b>",
             "sInfoFiltered": "(filter {{ __("labels.table.from") }} _MAX_ total data)",
             "sZeroRecords": "{{ __("labels.table.not_found") }}",
@@ -277,7 +277,7 @@
         ],
         "sPaginationType": "simple_numbers",
         ajax: '/sale/index_data_baki/{{$id}}',
-        dom: 'lfrtip',
+         dom: '<"row"<"col-md-2"l><"col-md-6"f>>rtip',
         // dom: 'Blfrtip',
         columns: [{
             "data": 'id',
